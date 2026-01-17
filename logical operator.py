4e6945dbd37390ba6c089baa4851bb6b4adc63b2
Gaurav02004 <girigaurav367@gmail.com>
2026-01-17 12:42:07 +0530

    Add files via upload

---
+++ logical operator.py
@@ -0,0 +1,33 @@
+# age = int(input("Enter your age: "))
+# if age < 18 or age >=35:
+#     print("You are not eligible for the marathon.")
+
+
+## EQUALITY OPERATOR
+
+# age = int(input("Enter your age: "))
+# if  age < 18:
+#     print("You are in the teenager age.")
+
+
+## COMPARISON OPERATOR
+
+# marks = float(input("Enter the marks"))
+# if marks >= 35:
+#      print("pass")
+# elif marks>= 50 and marks <=70:
+#      print("first")
+# elif marks<35:
+#      print("fail")
+
+
+## ARITHMETIC OPERATOR
+
+24+34
+
+
+
+
+
+    
+          
