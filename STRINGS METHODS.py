57fb28d63068e8b2fee7dbcaae498b0884031163
Gaurav02004 <girigaurav367@gmail.com>
2026-01-18 20:45:20 +0530

    Add files via upload

---
+++ STRINGS METHODS.py
@@ -0,0 +1,45 @@
+# STRINGS ARE MUTABLE IN PYTHON
+# a = "Gaurav"
+# print(len(a))
+# print(a.upper())
+# print(a.lower())
+
+# a = "!!!Gaurav!!!!!!!!!"
+# print(len(a))
+# print(a)
+# print(a.upper())
+# print(a.lower())
+# print(a.rstrip("!"))
+# print(a.replace("Gaurav","Ankita"))
+# print(a.split(" "))
+
+
+# STRINGS ARE IMMUTABLE IN PYTHON
+# a = "Gaurav"
+# print(a)
+# a[0] = "g"
+# print(a)
+
+
+name = "Gaurav"
+friend = "SHIZU"
+anotherfriend = "ANKITA SINGH"
+print("Hello," + name)
+print("Hello," + friend)
+print("Hello," + anotherfriend)
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[3])
+print(name[4])
+print(name[5])
+print(name[0:4])
+print(name[1:5])
+print(name[0:])
+print(name[:6])
+print(name[0::2])
+print(name[::2])
+print(name[::-1])
+
+
+
