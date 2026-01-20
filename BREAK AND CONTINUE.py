2946052c07e4386c20970091cc4b8ad7274b8284
Gaurav02004 <girigaurav367@gmail.com>
2026-01-20 19:59:53 +0530

    Add files via upload

---
+++ BREAK AND CONTINUE.py
@@ -0,0 +1,20 @@
+# for i in range(12):
+#     print("5 X",i+1,"=", 5 * (i+1))
+#     break
+#     if(i == 10):
+    
+# print("Loop ko chodkar nikal gya")
+
+
+# for i in range(12):
+#     if( i==10):
+#         print("Skip the iteration")
+#         continue
+#     print("5 X",i+1,"=", 5 * (i+1))
+
+i = 0
+while True:
+    print(i)
+    i = i + 1
+    if(i%100 == 0):
+        break
