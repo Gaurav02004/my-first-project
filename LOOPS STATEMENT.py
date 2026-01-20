41e85515596c07e6d51ee24ec0b694c60ac8db28
Gaurav02004 <girigaurav367@gmail.com>
2026-01-20 19:24:34 +0530

    Add files via upload

--- LOOPS STATEMENT.py
+++ LOOPS STATEMENT.py
@@ -17,14 +17,44 @@
 #     print(sum)
 
 ## FIND THE SUM OF EVEN AND ODD NUMBERS
-first = [1,2,3,4,5,6,7,8,9,10]
-even_sum = 0
-odd_sum = 0
-for i in first:
-    if(i % 2 == 0):
-        even_sum = even_sum + i
-    else:
-        odd_sum = odd_sum + i
-print("Sum of even numbers:", even_sum)
-print("Sum of odd numbers:", odd_sum)
+# first = [1,2,3,4,5,6,7,8,9,10]
+# even_sum = 0
+# odd_sum = 0
+# for i in first:
+#     if(i % 2 == 0):
+#         even_sum = even_sum + i
+#     else:
+#         odd_sum = odd_sum + i
+# print("Sum of even numbers:", even_sum)
+# print("Sum of odd numbers:", odd_sum)
 
+# print(1) 
+# print(2)
+# print(3)
+# print(4)
+# print(5)
+# print(6)
+# print(7)
+# print(9)
+# print(10)
+
+# name = "Python"
+# for i in name:
+#     print(i)
+     
+# if (i=="y"):
+#     print("This is something speecial")
+
+# colors =["red","green","blue","yellow"]
+# for color in colors:
+#     print(color)    
+#     for i in color:
+#         print(i)
+
+# for k in range(5):
+#     print(k+1)
+#     for k in range(1,20001):
+#                     print(k)
+
+for k in range(1,12,3):
+    print(k)
