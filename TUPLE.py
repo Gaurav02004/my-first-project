9ca0dc3a4aa65fb5fccd53f850ec0bf1a83786b9
Gaurav02004 <girigaurav367@gmail.com>
2026-01-23 00:49:01 +0530

    Add files via upload

---
+++ TUPLE.py
@@ -0,0 +1,19 @@
+tup = (1,5,7,"green",True)
+# tup[0] = 90
+print(type(tup),tup)
+print(len(tup))
+print(tup[0])
+print(tup[1])
+print(tup[2])
+print(tup[3])
+print(tup[4])
+
+if 3421 in tup:
+    print("Yes 342 is present in this tuple")
+    tup2 = tup[1:4]
+    print(tup2)
+
+
+
+
+
