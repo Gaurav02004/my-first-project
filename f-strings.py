bb49efafcbf597c5ab14552841674cb2286f9f56
Gaurav02004 <girigaurav367@gmail.com>
2026-01-23 01:16:22 +0530

    Add files via upload

---
+++ f-strings.py
@@ -0,0 +1,10 @@
+letter = "Hey my name is {1} and I am from {0}"
+country = "India"
+name = "Harry"
+print(letter.format(country,name))
+print(f"Hey my name is {name} and I am from {country}")
+
+txt = "For only {price:2f}dollars!"
+"Hey my name is {1} and I am from {0}"
+print(txt.format(price=49.09999))
+print(txt)
