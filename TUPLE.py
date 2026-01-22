65b3b5441ee4a2915d202bfa3f5e9b5513549331
Gaurav02004 <girigaurav367@gmail.com>
2026-01-23 00:52:44 +0530

    Add files via upload

--- TUPLE.py
+++ TUPLE.py
@@ -1,17 +1,23 @@
-tup = (1,5,7,"green",True)
-# tup[0] = 90
-print(type(tup),tup)
-print(len(tup))
-print(tup[0])
-print(tup[1])
-print(tup[2])
-print(tup[3])
-print(tup[4])
+# tup = (1,5,7,"green",True)
+# # tup[0] = 90
+# print(type(tup),tup)
+# print(len(tup))
+# print(tup[0])
+# print(tup[1])
+# print(tup[2])
+# print(tup[3])
+# print(tup[4])
 
-if 3421 in tup:
-    print("Yes 342 is present in this tuple")
-    tup2 = tup[1:4]
-    print(tup2)
+# if 3421 in tup:
+#     print("Yes 342 is present in this tuple")
+#     tup2 = tup[1:4]
+#     print(tup2)
+
+# OPERAION ON TUPLES
+
+tuple1 = (0,1,2,3,4,5,6,7,8,9)
+res = tuple1.index(3,4,8)
+print('count of 3 in tuple is:',res)
 
 
 
