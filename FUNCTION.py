0c258b84b66b1f74f5d99d89647ac1be347592cc
Gaurav02004 <girigaurav367@gmail.com>
2026-01-22 21:02:22 +0530

    Add files via upload

---
+++ FUNCTION.py
@@ -0,0 +1,32 @@
+# a = 9
+# b = 8
+# gmean1 = (a*b)/(a+b)
+# print(gmean1)
+# c = 8
+# d = 7
+
+# gmean2 = (c*d)/(c+d)
+# print(gmean2)
+ 
+# a = 8
+# b = 9
+# if(a>b):
+#     print("First number is greater")
+# else:
+#     print("Second number is greater or equal")
+#    calculateGmean(a,b)
+#     c = 8
+#     d = 74
+#     if(c>d):
+    
+
+
+def calculateGmean(a,b)
+    mean(a*b)/(a+b)
+    print(mean)
+
+def isGreater(a,b)
+    if(a>b)
+    print("First number is greater")
+else:
+print("Second number is greater or equal")      
