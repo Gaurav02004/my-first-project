222fc5db0420906b24d8a9e0f79ef9066e2708f2
Gaurav02004 <girigaurav367@gmail.com>
2026-01-23 00:39:31 +0530

    Add files via upload

---
+++ LIST METHODS.py
@@ -0,0 +1,23 @@
+l = [1,2,3,4,5]
+print(l)
+# l.append(7)
+# print(l)
+# l.sort()
+# print(l)
+# l.sort(reverse=True)
+# print(l)
+# l.reverse()
+# print(l.index(1))
+# print(l)
+# print(l.index(1))
+# m = l.copy()
+# m[0] = 0
+# print(l)
+# l.insert(1,899)
+# print(l)
+
+m = [900,1000,1100]
+k = l + m
+print(k)
+l.extend(m)
+print(l)
