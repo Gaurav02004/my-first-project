4d027847c9704628b54f4e593148d9bf799b6388
Gaurav02004 <girigaurav367@gmail.com>
2026-01-24 01:03:08 +0530

    Add files via upload

---
+++ SET METHODS.py
@@ -0,0 +1,29 @@
+# s = {1,2,3,4}
+# s2 = {3,6,7}
+# print(s.union(s2))
+# s.update(s2)
+# print(s,s2)
+
+
+cities = {"Tokyo","Madrid","Berlin","Delhi"}
+# cities2 = {"Tokyo","Seoul","Kabul","Madrid"}
+# cities3 = cities.union(cities2)
+# print(cities3)
+# cities3 = cities.intersection(cities2)
+# print(cities)
+# cities3 = cities.symmetric_difference(cities2)
+# print(cities3)
+# cities3 = cities.difference(cities2)
+# print(cities3)
+# cities3 = cities.isdisjoint(cities2)
+# cities3 = cities.issuperset(cities3)
+# print(cities.issuperset(cities3))
+
+# cities.remove("Tokyo")
+# print(cities)
+
+# cities.discard("Tokyo")
+# print(cities)
+
+del cities
+print(cities)
