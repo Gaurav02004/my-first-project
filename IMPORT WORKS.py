605985aa8cf7ef0a5a865dcd1da758a3b47f7a42
Gaurav02004 <girigaurav367@gmail.com>
2026-01-25 22:15:56 +0530

    Add files via upload

---
+++ IMPORT WORKS.py
@@ -0,0 +1,36 @@
+# import pandas
+# pandas.read_csv()
+
+# import math
+# math.floor(4.2343)
+
+# import math
+# result = math.sqrt(9)
+# print(result)
+
+# from math import sqrt,pi
+# result = sqrt(9) * pi
+# print(result)
+
+
+# from math import pi,sqrt as s
+
+# result = s(9) * pi
+# print(result)
+
+
+# from math import pi,sqrt as s
+
+# result = m.sqrt(9) * m.pi
+# print(result)
+
+from harry import welcome,harry
+import math
+print(dir(math))
+print(math.nan,type(math.nan))
+
+
+def  welcome():
+ print("Hey you are welcome  my friend")
+
+harry = "A good boy"
