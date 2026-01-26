c09d9a4d8feba7c648d02c775bddaf12f4e5f005
Gaurav02004 <girigaurav367@gmail.com>
2026-01-26 20:37:33 +0530

    Add files via upload

---
+++ MAP,FILTER AND REDUCE.py
@@ -0,0 +1,28 @@
+# MAP 
+# def cube(x):
+#     return x*x*x
+# print(cube(2))
+# l = [1,2,4,6,4,3]
+# # newl = []
+# # for item in l:
+# #     newl.append(cube(item))
+# newl = list(map(cube,l))
+# print(newl)
+
+
+# FILTER
+# def filter_function(a):
+#     return a>4
+# newnewl = filter(filter_function, l)
+# print(newlnewl)
+
+
+# REDUCE 
+
+from functool import reduce
+numbers = [1,2,3,4,5]
+numbers = [10,5]
+def mysum(x,y):
+    return x+y
+print(sum)
+
