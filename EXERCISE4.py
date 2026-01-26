5178eccf6127cbd8630a7e446484409b51999a31
Gaurav02004 <girigaurav367@gmail.com>
2026-01-26 13:27:57 +0530

    Add files via upload

---
+++ EXERCISE4.py
@@ -0,0 +1,25 @@
+# Your program should ask weather you want to code  or decode
+
+st = input("Enter message" )
+words = st.split("")
+coding = True
+if(coding):
+    nwords =[]
+    for word in words:
+        if(len(word)>=3):
+            r1 = "dsf"
+            r2 =  "jkr"
+            stnew = r1+word[1:] + word[0] + r2
+            nwords.append(stnew)
+            print("".join(nwords))
+
+        else:
+            nwords =[]
+            for word in words:
+                if(len(word)>=3):
+                    stnew = word[3:-3]
+                    stnew = stnew[-1] + stnew[:-1]
+                    nwords.append(stnew)
+                else:
+                    nwords.append(word[::-1])
+                    print("".join(nwords))
