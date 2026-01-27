239d7e709711a528372a730dcd7e9d264ad2b2ec
Gaurav02004 <girigaurav367@gmail.com>
2026-01-27 20:50:44 +0530

    Add files via upload

---
+++ CONSTRUCTOR.py
@@ -0,0 +1,20 @@
+class Person:
+  def__init__(self):
+print("Hey I am a person")
+    # self.name = name
+name = "Harry"
+occu = "Developer"
+
+def info(self):
+        print(f"{self.name, self.occu}")
+
+a = Person("Harry", "Developer")
+b = Person("Divya", "HR")    
+c = Person(1,2,3)  
+
+a = person()
+# print(a.name)        
+print(a.info)
+a.name = "Divya"
+a.occu = "HR"
+a.info()
