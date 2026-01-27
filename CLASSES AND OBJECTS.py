3be421ba218ae905e46cadefeba1dc9f8f59227e
Gaurav02004 <girigaurav367@gmail.com>
2026-01-27 20:26:22 +0530

    Add files via upload

---
+++ CLASSES AND OBJECTS.py
@@ -0,0 +1,20 @@
+class Person:
+name = "Harry"
+occupation = "Software Developer"
+networth = 10
+def info(self):
+ print(f"{self.name} is a {self.occupation}")
+        
+
+
+a = Person()
+a.name = "Shubham"
+a.occupation = "Accountant"
+print(a.name, a.occupation)
+     
+
+b.name = "Nikita"
+b.occupation = "HR"
+
+
+
