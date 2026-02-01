6d3d2ec602c7a77a3de212eb0f8b1679be356ddf
Gaurav02004 <girigaurav367@gmail.com>
2026-02-01 19:34:14 +0530

    Add files via upload

---
+++ DECORATOR.py
@@ -0,0 +1,18 @@
+print("decorator")
+print("Hello world")
+
+def greet(fx):
+    def mfx():
+     print("Good morning")
+    fx()
+    print("Thanks for using this function")
+    return mfx
+
+@greet
+def hello():
+    print("Hello world")
+
+def add(a,b):
+    print(a+b)
+
+hello()
