dbed30c7b80ccb55d87facef28b2f4c57c65c5d7
Gaurav02004 <girigaurav367@gmail.com>
2026-02-03 20:59:17 +0530

    Add files via upload

---
+++ ACCESS SPECIFIER.py
@@ -0,0 +1,33 @@
+# class Employee:
+#     def __init__(self):
+#      self.name = "Harry"
+
+
+# a = Employee()
+# print(a.__Employee__name)
+# print(a.__dir__())
+
+
+class student:
+    def __init__(self):
+
+     def _funName(self):
+
+        return "CodeWithHarry"
+     
+class  Subject(student):
+   
+   pass
+obj = student()
+obj1 = Subject()
+print(dir(obj))
+
+print(obj.__name)
+
+
+print(obj._funName())
+print(obj1._name)
+print(obj1._funName())
+
+
+
